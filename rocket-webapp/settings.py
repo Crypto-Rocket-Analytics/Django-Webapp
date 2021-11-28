f870a3cd6a1537cd1736b3958f4c38f7d4c363e6
XingyuLi20 <lixingyu@berkeley.edu>
2021-11-27 23:00:02 -0800

    change STATIC_ROOT

--- rocket-webapp/settings.py
+++ rocket-webapp/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = BASE_DIR
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
